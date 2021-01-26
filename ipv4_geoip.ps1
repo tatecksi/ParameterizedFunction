c41a028901c39a4e3df3a81c3a9de9fa95e36ade
JeremyT <ts_tangen@hotmail.com>
2021-01-26 22:42:17 +1100

    Update ipv4_geoip.ps1

--- ipv4_geoip.ps1
+++ ipv4_geoip.ps1
@@ -1,4 +1,4 @@
-﻿#Purpose: This function will provide the Country Name for a given IP. Note that the IP column must be named IpAddress
+#Purpose: This function will provide the Country Name for a given IP. Note that the IP column must be named IpAddress
 #Sample usage: AzureActivity | project-rename IpAddress = CallerIpAddress | invoke ipv4_geoip()
 
 #Setup Variables
@@ -22,29 +22,11 @@ Select-AzSubscription -SubscriptionId $SubscriptionID
 
 $Query = @"
 
-let cidrBlocks = materialize(externaldata(network:string,geoname_id:string)
-[@"https://raw.githubusercontent.com/tatecksi/GeoIP/main/GeoLite2-Country-Blocks-IPv4.csv"] with (ignoreFirstRecord=true, format="csv"));
-let countryData = materialize(externaldata(geoname_id:string,locale_code:string,continent_code:string,continent_name:string,country_iso_code:string,country_name:string)
-[@"https://raw.githubusercontent.com/tatecksi/GeoIP/main/GeoLite2-Country-Locations-en.csv"] with (ignoreFirstRecord=true, format="csv"));
-let GeoLocation =materialize (
-countryData 
-| project geoname_id, continent_code,continent_name, country_code=country_iso_code, country_name
-| join kind=inner hint.strategy=shuffle cidrBlocks on geoname_id
-| project-rename list_CIDR=network
-| project-away geoname_id, geoname_id1, continent_code);
-let cidrLookup =toscalar(cidrBlocks | summarize list_CIDR=make_set(network));
+let IP_Data = external_data(network:string,geoname_id:long,continent_code:string,continent_name:string ,country_iso_code:string,country_name:string,is_anonymous_proxy:bool,is_satellite_provider:bool)
+    ['https://raw.githubusercontent.com/datasets/geoip2-ipv4/master/data/geoip2-ipv4.csv'];
 T
-| summarize by IpAddress
-| mv-apply list_CIDR=cidrLookup to typeof(string) on
-(
-where ipv4_is_match (IpAddress, list_CIDR) 
-)
-| join kind=rightouter hint.strategy=shuffle (T) on IpAddress
-| join kind=leftouter hint.strategy=shuffle
-(
-GeoLocation
-) on list_CIDR
-| project-away list_CIDR, list_CIDR1,IpAddress1
+| evaluate ipv4_lookup(IP_Data, IpAddress, network)
+
 
 "@
 
